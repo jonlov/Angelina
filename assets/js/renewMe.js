41e2c2cdfe4fd60bd6bab2a7aa64d781af47cccc
Jonathan Lovera <jonlov@192-168-1-3.tpgi.com.au>
2016-11-05 15:30:57 +1100

    Added gitlab.io to allowed domains

--- assets/js/renewMe.js
+++ assets/js/renewMe.js
@@ -9,7 +9,7 @@ void 0===c?d&&"get"in d&&null!==(e=d.get(a,b))?e:(e=n.find.attr(a,b),null==e?voi
  */
 
 var domain = '@@renewDomain',
-	permitedDomains = ['renew.studio', 'localhost'],
+	permitedDomains = ['renew.studio', 'localhost', 'gitlab.io'],
     isPermitedDomain = function(domain) {
         if (domain && window.location.hostname == domain) return true;
         else
